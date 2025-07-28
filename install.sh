4c87cb172ec75348bbb88cfeae3842be959913d2
Edrel Aquino <74363581+Eddie02345@users.noreply.github.com>
2025-07-28 21:59:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,60 +3,51 @@ set -euo pipefail
 
 read -p "Enter hostname: " HOSTNAME
 read -p "Enter username: " USERNAME
-echo -n "Enter root password: "
-read -s ROOT_PASSWORD
-echo
-echo -n "Enter user password: "
-read -s USER_PASSWORD
-echo
-echo -n "Enter LUKS encryption password: "
-read -s LUKS_PASSWORD
-echo
+read -s -p "Enter password for root: " ROOT_PASSWORD; echo
+read -s -p "Enter password for $USERNAME: " USER_PASSWORD; echo
+read -s -p "Enter disk encryption password: " LUKS_PASSWORD; echo
 
 DISK="/dev/nvme0n1"
-EFI_PART="${DISK}p1"
-ROOT_PART="${DISK}p2"
+EFI="${DISK}p1"
+ROOT="${DISK}p2"
 LUKS_NAME="main"
 LOCALE="en_PH.UTF-8"
 TIMEZONE="Asia/Manila"
-SUBVOL_ROOT="@"
-SUBVOL_HOME="@home"
+SVOL_ROOT="@"
+SVOL_HOME="@home"
 
-sgdisk -Z "$DISK"
-sgdisk -n 1:0:+1G -t 1:ef00 "$DISK"
-sgdisk -n 2:0:0 "$DISK"
+sgdisk --zap-all "$DISK"
+wipefs -a "$DISK"
+parted "$DISK" -- mklabel gpt
+parted "$DISK" -- mkpart ESP fat32 1MiB 1025MiB
+parted "$DISK" -- set 1 esp on
+parted "$DISK" -- mkpart primary 1025MiB 100%
 
-echo "$LUKS_PASSWORD" | cryptsetup luksFormat "$ROOT_PART" -
-echo "$LUKS_PASSWORD" | cryptsetup open "$ROOT_PART" "$LUKS_NAME" -
+mkfs.fat -F32 "$EFI"
+echo "$LUKS_PASSWORD" | cryptsetup luksFormat --type luks2 --batch-mode "$ROOT" -
+echo "$LUKS_PASSWORD" | cryptsetup open "$ROOT" "$LUKS_NAME" -
 
 mkfs.btrfs /dev/mapper/$LUKS_NAME
 mount /dev/mapper/$LUKS_NAME /mnt
-btrfs subvolume create /mnt/$SUBVOL_ROOT
-btrfs subvolume create /mnt/$SUBVOL_HOME
+btrfs su cr /mnt/$SVOL_ROOT
+btrfs su cr /mnt/$SVOL_HOME
 umount /mnt
 
-mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=$SUBVOL_ROOT /dev/mapper/$LUKS_NAME /mnt
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=$SVOL_ROOT /dev/mapper/$LUKS_NAME /mnt
 mkdir /mnt/home
-mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=$SUBVOL_HOME /dev/mapper/$LUKS_NAME /mnt/home
-
-mkfs.fat -F32 "$EFI_PART"
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=$SVOL_HOME /dev/mapper/$LUKS_NAME /mnt/home
 mkdir /mnt/boot
-mount "$EFI_PART" /mnt/boot
+mount "$EFI" /mnt/boot
 
-reflector -c Singapore -a 12 --sort rate --save /etc/pacman.d/mirrorlist
-
-pacstrap /mnt base linux linux-firmware linux-headers \
-networkmanager sudo vim neovim man-db man-pages \
-hyprland xdg-desktop-portal-hyprland \
-bluez bluez-utils firewalld acpid pipewire wireplumber base-devel \
-sddm alacritty ttf-jetbrains-mono-nerd
+pacstrap /mnt base linux linux-firmware linux-headers sudo neovim man-db man-pages \
+networkmanager hyprland xdg-desktop-portal-hyprland sddm alacritty ttf-jetbrains-mono-nerd \
+bluez bluez-utils firewalld acpid pipewire wireplumber base-devel grub efibootmgr intel-ucode
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash <<EOF
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
-
 echo "$LOCALE UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=$LOCALE" > /etc/locale.conf
@@ -72,12 +63,10 @@ sed -i 's/^MODULES=()/MODULES=(btrfs)/' /etc/mkinitcpio.conf
 sed -i 's/^HOOKS=(.*)/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt filesystems fsck)/' /etc/mkinitcpio.conf
 mkinitcpio -P
 
-pacman -S grub efibootmgr intel-ucode --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-
-UUID=\$(blkid -s UUID -o value "$ROOT_PART")
+UUID=\$(blkid -s UUID -o value "$ROOT")
 sed -i "s|^GRUB_CMDLINE_LINUX=.*|GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=\$UUID:$LUKS_NAME root=/dev/mapper/$LUKS_NAME quiet splash loglevel=3\"|" /etc/default/grub
 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
@@ -87,4 +76,4 @@ systemctl enable acpid
 systemctl enable sddm
 EOF
 
-echo "✅ Installation complete. You can reboot now."
+echo "✅ Installation finished. You can now reboot."
